7babbaf7371c606a97bb11b53148b1f5515eea2d
Draycia <lonelyyordle@gmail.com>
2021-01-11 20:44:22 -0800

    System.err as well. Closes #9
    Kinda a dumb and hacky way to do this lol

--- src/main/java/net/draycia/sysoutcatcher/SysoutCatcher.java
+++ src/main/java/net/draycia/sysoutcatcher/SysoutCatcher.java
@@ -9,7 +9,7 @@ public final class SysoutCatcher extends JavaPlugin {
     private static final StackWalker stackWalker = StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE);
 
     public SysoutCatcher() {
-        PrintStream myStream = new PrintStream(System.out) {
+        PrintStream wrappedOut = new PrintStream(System.out) {
             @Override
             public void println(String line) {
 
@@ -36,7 +36,35 @@ public final class SysoutCatcher extends JavaPlugin {
             }
         };
 
-        System.setOut(myStream);
+        PrintStream wrappedErr = new PrintStream(System.err) {
+            @Override
+            public void println(String line) {
+
+                try {
+                    Class<?> caller = stackWalker.getCallerClass();
+
+                    StringBuilder messageBuilder = new StringBuilder();
+
+                    if (getConfig().getBoolean("IncludeSourceClass", false)) {
+                        messageBuilder.append('(').append(caller.getName()).append(") ");
+                    }
+                    if (getConfig().getBoolean("IncludeSourceLine", false)) {
+                        messageBuilder.append('(').append(new Exception().getStackTrace()[2].getLineNumber()).append(") ");
+                    }
+
+                    messageBuilder.append(line);
+
+                    JavaPlugin.getProvidingPlugin(caller).getLogger().severe(messageBuilder.toString());
+                } catch (IllegalArgumentException e) {
+                    StackTraceElement element = new Exception().getStackTrace()[2];
+                    super.printf("(%s:%d) %s\n", element.getClassName(), element.getLineNumber(), line);
+                }
+
+            }
+        };
+
+        System.setOut(wrappedOut);
+        System.setErr(wrappedErr);
     }
 
     @Override
