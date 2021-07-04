2270532c399345f05d7156b0f666991f00fff284
Draycia <lonelyyordle@gmail.com>
2021-07-04 12:51:17 -0700

    Use logger everywhere, supprt bukkit logger

--- src/main/java/net/draycia/sysoutcatcher/SysoutCatcher.java
+++ src/main/java/net/draycia/sysoutcatcher/SysoutCatcher.java
@@ -1,10 +1,14 @@
 package net.draycia.sysoutcatcher;
 
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.PrintStream;
 
-public final class SysoutCatcher extends JavaPlugin {
+public final class SysoutCatcher extends JavaPlugin implements Listener {
 
     public SysoutCatcher() {
         // Listen to System.out.println
@@ -35,7 +39,7 @@ public final class SysoutCatcher extends JavaPlugin {
                 } catch (ClassNotFoundException | IllegalArgumentException e) {
                     // If anything happens, the calling class doesn't exist, there is no JavaPlugin that "owns" the calling class, etc
                     // Just print out normally, with some added information
-                    super.printf("(%s:%d) %s\n", element.getClassName(), element.getLineNumber(), line);
+                    getLogger().info(String.format("(%s:%d) %s\n", element.getClassName(), element.getLineNumber(), line));
                 }
             }
         };
@@ -68,7 +72,7 @@ public final class SysoutCatcher extends JavaPlugin {
                 } catch (ClassNotFoundException | IllegalArgumentException e) {
                     // If anything happens, the calling class doesn't exist, there is no JavaPlugin that "owns" the calling class, etc
                     // Just print out normally, with some added information
-                    super.printf("(%s:%d) %s\n", element.getClassName(), element.getLineNumber(), line);
+                    getLogger().severe(String.format("(%s:%d) %s\n", element.getClassName(), element.getLineNumber(), line));
                 }
             }
         };
@@ -76,6 +80,45 @@ public final class SysoutCatcher extends JavaPlugin {
         // Set the two PrintStreams so they're used
         System.setOut(wrappedOut);
         System.setErr(wrappedErr);
+
+        Bukkit.getLogger().setUseParentHandlers(false);
+
+        Bukkit.getLogger().addHandler(new Handler() {
+            @Override
+            public void publish(LogRecord record) {
+                if (record == null) return;
+
+                try {
+                    // Get the class name at that index and the JavaPlugin that "owns" it
+                    Class<?> clazz = Class.forName(record.getSourceClassName());
+                    JavaPlugin plugin = JavaPlugin.getProvidingPlugin(clazz);
+
+                    StringBuilder messageBuilder = new StringBuilder();
+
+                    // Optional information that may be added to the logged line
+                    if (getConfig().getBoolean("IncludeSourceClass", false)) {
+                        messageBuilder.append('(').append(record.getSourceClassName()).append(") ");
+                    }
+                    if (getConfig().getBoolean("IncludeSourceLine", false)) {
+                        messageBuilder.append('(').append(new Exception().getStackTrace()[2].getLineNumber()).append(") ");
+                    }
+
+                    messageBuilder.append(record.getMessage());
+
+                    // Instead of just printing the message, send it to the plugin's logger
+                    plugin.getLogger().log(record.getLevel(), messageBuilder.toString());
+                } catch (ClassNotFoundException | IllegalArgumentException e) {
+                    // If anything happens, the calling class doesn't exist, there is no JavaPlugin that "owns" the calling class, etc
+                    getLogger().log(record);
+                }
+            }
+
+            @Override
+            public void flush() { }
+
+            @Override
+            public void close() throws SecurityException { }
+        });
     }
 
     @Override
